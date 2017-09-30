1169fc1ee899034f6d1d8653aee5eaee7e973833
manas daruka <manas.daruka@gmail.com>
2017-09-30 16:24:54 +0530

    Completed program_name

--- shell.c
+++ shell.c
@@ -128,6 +128,78 @@ void fileIO(char *args[], char *inputFile, char *outputFile, int option) {
     waitpid(pid, NULL, 0);
 }
 
+pid_t child_pid;
+
+void kill_child(int sig)
+{
+	kill(child_pid,SIGKILL);
+}
+
+void fileIO_limited(char *args[], char *inputFile, char *outputFile, int option,int m) {
+
+    int err = -1;
+
+    int fileDescriptor; // between 0 and 19, describing the output or input file
+    signal(SIGALRM,(void (*)(int))kill_child);
+    child_pid = fork();
+    if (child_pid == -1) {
+        printf("Child process could not be created\n");
+        return;
+    }
+    if (child_pid == 0) {
+        // Option 0: output redirection
+        if (option == 0) {
+            // We open (create) the file truncating it at 0, for write only
+            fileDescriptor = open(outputFile, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+            // We replace de standard output with the appropriate file
+            dup2(fileDescriptor, STDOUT_FILENO);
+            close(fileDescriptor);
+            // Option 1: input and output redirection
+        } else if (option == 1) {
+            // We open file for read only (it's STDIN)
+            fileDescriptor = open(inputFile, O_RDONLY, 0600);
+            // We replace de standard input with the appropriate file
+            dup2(fileDescriptor, STDIN_FILENO);
+            close(fileDescriptor);
+            // Same as before for the output file
+            fileDescriptor = open(outputFile, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+            dup2(fileDescriptor, STDOUT_FILENO);
+            close(fileDescriptor);
+        }
+
+        setenv("parent", getcwd(currentDirectory, 1024), 1);
+
+        if (execvp(args[0], args) == err) {
+            printf("err");
+            kill(getpid(), SIGTERM);
+        }
+    }
+    alarm(m);
+    waitpid(child_pid, NULL, 0);
+}
+
+void print_to_stdout(){
+    FILE *fptr;
+    char c;
+    
+       // Open file
+       fptr = fopen("std_output.txt", "r");
+       if (fptr == NULL)
+       {
+           printf("Cannot open file \n");
+           exit(0);
+       }
+    
+       // Read contents from file
+       c = fgetc(fptr);
+       while (c != EOF)
+       {
+           printf ("%c", c);
+           c = fgetc(fptr);
+       }
+       fclose(fptr);
+}
+
 /*
  * Remove Function */
 int removeFile(char *file) {
@@ -407,19 +479,49 @@ int commandHandler(char *args[]) {
     else {
         // If none of the preceding commands were used, we invoke the
         // specified program. We have to detect I/O redirection.
-        while (args[i] != NULL) {
+        int m,m_ind;
+        for(i=0;;i++)
+        {
+            if(args[i+1] == NULL){
+            m_ind=i;
+            m=atoi(args[i]);
+            break;
+            }
+        }
+        i=j;
+        //printf("%d",m);
+        if(m==0)
+        m_ind=-10;
+        while (args[i] != NULL && m_ind!=i) {
             if (strcmp(args[i], "<") == 0) {
                 aux = i + 1;
-                if (args[aux] == NULL || args[aux + 1] == NULL || args[aux + 2] == NULL) {
+                if(args[i+1] == NULL){
                     printf("Not enough input arguments\n");
                     return -1;
+                }
+
+                if (args[i+2] == NULL || i+2==m_ind) {
+                    if(m==0)
+                    fileIO(args_aux, args[i+1], "std_output.txt", 1);
+                    else
+                    fileIO_limited(args_aux, args[i+1], "std_output.txt", 1,m);
+                    //print output.txt
+                    print_to_stdout();
+                    return 1;
                 } else {
-                    if (strcmp(args[aux + 1], ">") != 0) {
+                    if (strcmp(args[i + 1], ">") != 0) {
                         printf("Usage: Expected '>' and found %s\n", args[aux + 1]);
                         return -2;
                     }
+                    else if(args[i+3] == NULL || m_ind==i+3){
+                        printf("Not enough output arguments\n");
+                        return -1;
+                    }
                 }
+                if(m==0)
                 fileIO(args_aux, args[i + 1], args[i + 3], 1);
+                else
+                fileIO_limited(args_aux, args[i + 1], args[i + 3], 1, m);
                 return 1;
             }
                 // If '>' is detected, we have output redirection.
@@ -430,10 +532,20 @@ int commandHandler(char *args[]) {
                     printf("Not enough input arguments\n");
                     return -1;
                 }
+                if(m==0)
                 fileIO(args_aux, NULL, args[i + 1], 0);
+                else
+                fileIO_limited(args_aux, NULL, args[i + 1], 0, m);
                 return 1;
             }
             i++;
+        }
+        if(i==j){
+            if(m==0)
+            fileIO(args_aux, NULL, "std_output.txt", 0);
+            else
+            fileIO_limited(args_aux, NULL, "std_output.txt",0, m);
+            print_to_stdout();
         }
     }
     return 1;
@@ -482,9 +594,9 @@ int main(int argc, char *argv[], char **envp) {
         // We read all the tokens of the input and pass it to our
         // commandHandler as the argument
         numTokens = 1;
-        // printf("%s\n", tokens[0]);
+        
         while ((tokens[numTokens] = strtok(NULL, " \n\t")) != NULL) numTokens++;
-
+        //printf("%d\n", numTokens);
         commandHandler(tokens);
         printf("\n");
 
---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int i,n;
+    scanf("%d",&n);
+    for(i=0;i<n;)
+    printf("check\n");
+    return 0;
+}
