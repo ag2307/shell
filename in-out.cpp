ca3c9bf2f89bed58e8a6d4baef0e711a131843be
Abhinav Gupta <abhinavgupta203@gmail.com>
2018-11-01 16:06:49 +0530

    added testing files

---
+++ in-out.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int i,j;
+	cin >> i >> j;
+	cout << i<< " " << j << endl;
+}
---
+++ infinite.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int i;
+	i=1;
+	while(1){
+		cout << i << endl;
+		i++;
+	}
+
+}
